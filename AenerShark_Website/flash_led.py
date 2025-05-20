0231c886b480147ee95c6193cf983bcd8ab39e9f
ryanyue-16 <YYH2015913923@126.com>
2025-05-20 15:53:43 +0100

    Update flash_led.py

--- AenerShark_Website/flash_led.py
+++ AenerShark_Website/flash_led.py
@@ -1,12 +1,11 @@
 import RPi.GPIO as GPIO
 import time
 
-LED_PIN = 4  # GPIO 4 (pin 7)
+LED_PIN = 4
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(LED_PIN, GPIO.OUT)
 
-# Flash the LED
 GPIO.output(LED_PIN, GPIO.HIGH)
 time.sleep(0.5)
 GPIO.output(LED_PIN, GPIO.LOW)
