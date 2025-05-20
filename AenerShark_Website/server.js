031c044dad0bc8eb616cf521b884432e6529d215
Antoine Divet <142180482+aa6dcc@users.noreply.github.com>
2025-05-20 16:26:31 +0100

    Update server.js

--- AenerShark_Website/server.js
+++ AenerShark_Website/server.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const path = require('path');
 
 const app = express();
-const PORT = 5000;
+const PORT = 6000;
 
 // Serve static files (index.html, assets, etc.)
 app.use(express.static(path.join(__dirname)));
