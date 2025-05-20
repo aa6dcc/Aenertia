6894a542d3abd5023972e3fb1bdcc17c41cfdf6b
ryanyue-16 <YYH2015913923@126.com>
2025-05-20 15:51:04 +0100

    Update script.js

--- AenerShark_Website/assets/js/script.js
+++ AenerShark_Website/assets/js/script.js
@@ -1,3 +1,10 @@
 function sayHello() {
   alert("Hi there! This is a JS alert.");
 }
+
+function flashLED() {
+  fetch('/flash-led')
+    .then(response => response.text())
+    .then(data => alert(data))
+    .catch(error => alert("Failed to flash LED: " + error));
+}
