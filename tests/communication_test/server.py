d8b84ad5aef80f0127bc0053a0eb56753412dfdf
BenoitBenM <benoitbenm@gmail.com>
2025-05-23 12:51:47 +0200

    Include a route for /

--- tests/communication_test/server.py
+++ tests/communication_test/server.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 import paho.mqtt.publish as publish
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
@@ -13,5 +14,15 @@ def control_led(id: int):
         publish.single("led/control", "OFF", hostname="localhost")
     return {"status": f"Command sent: LED{id}"}
 
+
+# Define a route for no / (test the UI before running mqtt)
+@app.get("/", response_class=HTMLResponse)
+def index():
+    return """
+    <h1>LED Control</h1>
+    <a href="/led/1"><button>LED 1</button></a>
+    <a href="/led/2"><button>LED 2</button></a>
+    <a href="/led/0"><button>OFF</button></a>
+    """
 
 # Start it using uvicorn led_api:app --host 0.0.0.0 --port 8000
