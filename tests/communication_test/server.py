50d3cc9e9e0b51ff88fd3e4fd8ace43c614a4d15
Antoine Divet <aa6dcc@gmail.com>
2025-05-27 11:50:34 +0100

    import Form line edit

--- tests/communication_test/server.py
+++ tests/communication_test/server.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Form
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 import paho.mqtt.publish as publish
