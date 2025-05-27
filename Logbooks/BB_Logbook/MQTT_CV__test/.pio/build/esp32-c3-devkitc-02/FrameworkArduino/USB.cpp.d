61dc2ade8e01035d347d175c23e567a20afad235
BenoitBenM <benoitbenm@gmail.com>
2025-05-27 14:08:55 +0200

    Made Test code work with simple UI

---
+++ Logbooks/BB_Logbook/MQTT_CV__test/.pio/build/esp32-c3-devkitc-02/FrameworkArduino/USB.cpp.d
@@ -0,0 +1,4 @@
+.pio/build/esp32-c3-devkitc-02/FrameworkArduino/USB.cpp.o: \
+ C:/Users/bb923/.platformio/packages/framework-arduinoespressif32/cores/esp32/USB.cpp \
+ C:/Users/bb923/.platformio/packages/framework-arduinoespressif32/cores/esp32/USB.h \
+ C:/Users/bb923/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/dio_qspi/include/sdkconfig.h
