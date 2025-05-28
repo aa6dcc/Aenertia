2c80477f977a6bbbace54526ab85d541c219efc6
BenoitBenM <benoitbenm@gmail.com>
2025-05-28 17:39:42 +0100

    Changed a million things

--- Logbooks/BB_Logbook/MQTT_CV_test/mqtt_serial_bridge.py
+++ Logbooks/BB_Logbook/MQTT_CV_test/mqtt_serial_bridge.py
@@ -6,13 +6,16 @@ from time import sleep
 import numpy as np
 import cv2
 
-#Serial configf
+#Serial config
 SERIAL_PORT = "/dev/ttyUSB0"
 baud_rate = 115200
 
+#global variables
 cv_enabled = False
 ser = None
 mode = "manual" #Global var mode initialized to Manual
+HumanDetected = False
+offset = 0
 
 
 def send_2_esp(command):
@@ -30,10 +33,16 @@ def fake_cv_loop(): # This loop will go until cv_disabled is called. Hozever bec
         send_2_esp("LED_OFF")
         sleep(2)
 
+
 def cv_loop():
+    global offset
     pass
+    '''
+    return 
+    '''
 
-
+def GotoKeyLocation():
+    print("ROS2 works ; Python stays silent")
 
 def on_connect(client, userdata, flags, rc):
     print("MQTT connected with result code", rc)
@@ -42,12 +51,11 @@ def on_connect(client, userdata, flags, rc):
     client.subscribe("robot/manual/command")
 
 
-
 def on_message(client, userdata, msg):
     global cv_enabled
     global mode
     payload = msg.payload.decode()
-    topic = msg.topic.decode()
+    topic = msg.topic
     print(f"Topic:{msg.topic} ; Command: {payload}")
 
     # We start by checking if the mode was changed
@@ -68,13 +76,16 @@ def on_message(client, userdata, msg):
             send_2_esp("forwardANDright")
         elif payload == "up-left":
             send_2_esp("forwardANDleft")
-        elif payload == "up-right":
+        elif payload == "down-right":
             send_2_esp("backwardANDright")
-        elif payload == "up-left":
+        elif payload == "down-left":
             send_2_esp("backwardANDleft")
+        else:
+            send_2_esp("stop")
 
     # Autnomous mode code
-    elif mode == "autonomous":    
+    elif mode == "autonomous":  
+        ''' 
         if topic == "robot/cv":
             if payload == "enable cv":
                 if not cv_enabled:
@@ -84,9 +95,40 @@ def on_message(client, userdata, msg):
             elif payload == "disable cv":
                 cv_enabled = False
                 send_2_esp("LED_OFF")
-        
+        '''
+        if topic == "robot/auto":
+            #This part of the code will use data from computer vision to decide wether the robot should go forward or backward
+            if payload == "follow":
+                if HumanDetected:
+                    if abs(offset) < 10: 
+                        send_2_esp("forward")
+                    elif 10 <= offset < 100:
+                        send_2_esp("forward and right")
+                    elif -10 >= offset > -100:
+                        send_2_esp("forzard and left")
+                    elif offset >= 100:
+                        send_2_esp("right")
+                    elif offset <= -100:
+                        send_2_esp("left")
+                    else: 
+                        send_2_esp("stop")
+                else :
+                    send_2_esp("stop")
+            
+            elif payload == "GotoKeyLocation":
+                GotoKeyLocation()
+
+            elif payload == "stop": #To be implemented later
+                send_2_esp("stop")
+            
+
+                    
+
 
 def main():
+    #Robot function
+    #Telemetry loop
+
     global ser
     ser = serial.Serial(SERIAL_PORT, baud_rate, timeout=1)
 
@@ -95,6 +137,7 @@ def main():
     client.on_message = on_message
     client.connect("localhost", 1883, 60)
     client.loop_forever()
+
 
 if __name__ == "__main__":
     main()
--- Logbooks/BB_Logbook/esp32_non_balancing_code/src/main.cpp
+++ Logbooks/BB_Logbook/esp32_non_balancing_code/src/main.cpp
@@ -3,10 +3,13 @@
 #include <TimerInterrupt_Generic.h>
 #include <step.h>
 
+#define LED_PIN 2
+
+
 // The Stepper pins
 const int STEPPER1_DIR_PIN  = 16;
 const int STEPPER1_STEP_PIN = 17;
-const int STEPPER2_DIR_PIN  = 4;
+const int STEPPER2_DIR_PIN  = 4; 
 const int STEPPER2_STEP_PIN = 14;
 const int STEPPER_EN_PIN    = 15; 
 
@@ -15,7 +18,8 @@ const int TOGGLE_PIN        = 32;
 const int PRINT_INTERVAL    = 300;
 const int LOOP_INTERVAL     = 10;
 const int STEPPER_INTERVAL_US = 20;
-
+float spd = 8.0;
+String mode = "manual";
 
 //Global objects
 ESP32Timer ITimer(3);
@@ -47,87 +51,111 @@ void setup()
  
   //Attach motor update ISR to timer to run every STEPPER_INTERVAL_US μs
   if (!ITimer.attachInterruptInterval(STEPPER_INTERVAL_US, TimerHandler)) {
-    Serial.println("Failed to start stepper interrupt");
+    //Serial.println("Failed to start stepper interrupt");
     while (1) delay(10);
     }
-  Serial.println("Initialised Interrupt for Stepper");
+  //Serial.println("Initialised Interrupt for Stepper");
 
-  step1.setAccelerationRad(10);
-  step2.setAccelerationRad(10);
+  step1.setAccelerationRad(20);
+  step2.setAccelerationRad(20);
 
-  Serial.println("Code started:");
+
+  //LED debug output
+  pinMode(LED_PIN, OUTPUT);
+  digitalWrite(LED_PIN, LOW);
+
+  //Serial.println("Code started:");
 
 
 }
 
 void loop()
 {
-
-/*
   if(Serial.available()) {
     String command = Serial.readStringUntil('\n');
     command.trim();
-
+  
+    // We start by checking if the mode was changed
+    
     if(command == "forward") {
-      step1.setTargetSpeedRad(100);
-      step2.setTargetSpeedRad(100);
+      step1.setTargetSpeedRad(spd);
+      step2.setTargetSpeedRad(spd);
+      Serial.println("forward");
     }
     else if (command == "backward") {
-      step1.setTargetSpeedRad(-100);
-      step2.setTargetSpeedRad(-100);
+      step1.setTargetSpeedRad(-spd);
+      step2.setTargetSpeedRad(-spd);
+      Serial.println("backward");
     }
     else if (command == "left") {
-      step1.setTargetSpeedRad(-100);
-      step2.setTargetSpeedRad(100);
+      step1.setTargetSpeedRad(-spd);
+      step2.setTargetSpeedRad(spd);
+      Serial.println("left");
     }
     else if (command == "right") {
-      step1.setTargetSpeedRad(100);
-      step2.setTargetSpeedRad(-100);
+      step1.setTargetSpeedRad(spd);
+      step2.setTargetSpeedRad(-spd);
+      Serial.println("right");
     }
 
     else if(command == "forwardANDleft") {
-      step1.setTargetSpeedRad(50);
-      step2.setTargetSpeedRad(100);
+      step1.setTargetSpeedRad(4*spd/11);
+      step2.setTargetSpeedRad(spd);
+
+      Serial.println("Forward and left");
+
     }
     else if (command == "forwardANDright") {
-      step1.setTargetSpeedRad(100);
-      step2.setTargetSpeedRad(50);
+      step1.setTargetSpeedRad(spd);
+      step2.setTargetSpeedRad(4*spd/11);
+
+      Serial.println("Forward and right");
     }
     else if (command == "backwardANDleft") {
-      step1.setTargetSpeedRad(-50);
-      step2.setTargetSpeedRad(-100);
+      step1.setTargetSpeedRad(-4*spd/11);
+      step2.setTargetSpeedRad(-spd);
+      Serial.println("Backward and left");
     }
     else if (command == "backwardANDright") {
-      step1.setTargetSpeedRad(-100);
-      step2.setTargetSpeedRad(-50);
+      step1.setTargetSpeedRad(-spd);
+      step2.setTargetSpeedRad(-4*spd/11);
+      Serial.println("Backward and right");
     }
     else if (command == "stop") {
       step1.setTargetSpeedRad(0);
       step2.setTargetSpeedRad(0);
+      Serial.println("Stop");
     }
   
   }
 
+/* test motion before serial
+  digitalWrite(LED_PIN, HIGH);
+  step1.setTargetSpeedRad(10);
+  step2.setTargetSpeedRad(10);
+  Serial.println("Forward");
+
+  delay(4000);
+
+  digitalWrite(LED_PIN, LOW);
+  step1.setTargetSpeedRad(0);
+  step2.setTargetSpeedRad(0);
+  Serial.println("Stop");
+
+  delay(4000);
+
+  digitalWrite(LED_PIN, HIGH);
+  step1.setTargetSpeedRad(-10);
+  step2.setTargetSpeedRad(10);
+  Serial.println("Backward");
+
+  delay(4000);
+
+  digitalWrite(LED_PIN, LOW);
+  step1.setTargetSpeedRad(0);
+  step2.setTargetSpeedRad(0);
+  Serial.println("Stop");
+  delay(4000);
 */
-
-// test motion before serial
-
-  step1.setTargetSpeedRad(1000);
-  step2.setTargetSpeedRad(1000);
-
-  delay(1000);
-
-  step1.setTargetSpeedRad(0);
-  step2.setTargetSpeedRad(0);
-
-  delay(1000);
-
-  step1.setTargetSpeedRad(-1000);
-  step2.setTargetSpeedRad(-1000);
-
-  delay(1000);
-
-  step1.setTargetSpeedRad(0);
-  step2.setTargetSpeedRad(0);
 
 }
