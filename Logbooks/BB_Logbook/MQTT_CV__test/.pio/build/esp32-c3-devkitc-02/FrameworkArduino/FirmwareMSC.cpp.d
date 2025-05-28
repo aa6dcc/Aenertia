16114ddb6ad3cab0d2bf76b2f4406bfec180bab7
BenoitBenM <148616495+BenoitBenM@users.noreply.github.com>
2025-05-28 17:42:14 +0100

    Delete Logbooks/BB_Logbook/MQTT_CV__test directory

--- Logbooks/BB_Logbook/MQTT_CV__test/.pio/build/esp32-c3-devkitc-02/FrameworkArduino/FirmwareMSC.cpp.d
+++
@@ -1,5 +0,0 @@
-.pio/build/esp32-c3-devkitc-02/FrameworkArduino/FirmwareMSC.cpp.o: \
- C:/Users/bb923/.platformio/packages/framework-arduinoespressif32/cores/esp32/FirmwareMSC.cpp \
- C:/Users/bb923/.platformio/packages/framework-arduinoespressif32/cores/esp32/FirmwareMSC.h \
- C:/Users/bb923/.platformio/packages/framework-arduinoespressif32/cores/esp32/USBMSC.h \
- C:/Users/bb923/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/dio_qspi/include/sdkconfig.h
--- Logbooks/BB_Logbook/MQTT_CV__test/.pio/build/esp32-c3-devkitc-02/FrameworkArduino/USBMSC.cpp.d
+++
@@ -1,4 +0,0 @@
-.pio/build/esp32-c3-devkitc-02/FrameworkArduino/USBMSC.cpp.o: \
- C:/Users/bb923/.platformio/packages/framework-arduinoespressif32/cores/esp32/USBMSC.cpp \
- C:/Users/bb923/.platformio/packages/framework-arduinoespressif32/cores/esp32/USBMSC.h \
- C:/Users/bb923/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/dio_qspi/include/sdkconfig.h
