bd2022d4d748900d78dbb13c28fc0523a7abaf0d
Antoine Divet <aa6dcc@gmail.com>
2025-05-29 18:07:23 +0100

    changed Decimal type

--- server/database/test_robot_state.py
+++ server/database/test_robot_state.py
@@ -1,5 +1,6 @@
 import boto3
 from datetime import datetime
+from decimal import Decimal
 
 dynamodb = boto3.resource('dynamodb', region_name='eu-north-1')
 table = dynamodb.Table('RobotState')
@@ -7,9 +8,9 @@ table = dynamodb.Table('RobotState')
 table.put_item(Item={
     'device_id': 'robot001',
     'timestamp': datetime.utcnow().isoformat(),
-    'x': 15.2,
-    'y': 30.5,
-    'battery': 87,
+    'x': Decimal('15.2'),
+    'y': Decimal('30.5'),
+    'battery': Decimal('87'),
     'mode': 'autonomous',
     'status': 'connected',
     'current_key_loc': 'ChargingStation'
