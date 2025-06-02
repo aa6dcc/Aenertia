b80d780e3e8878c27d4e2b1b5668c9c0c4817e2b
Antoine Divet <aa6dcc@gmail.com>
2025-06-02 11:39:57 +0100

    worked on dynamoDB

--- server/main.py
+++ server/main.py
@@ -1,1 +1,10 @@
 #server logic
+
+from database import test_robot_state, test_location_history, test_key_locations, test_pid_values
+
+# Example entrypoint to manually test all insertions
+if __name__ == "__main__":
+    test_robot_state.run()
+    test_location_history.run()
+    test_key_locations.run()
+    test_pid_values.run()
